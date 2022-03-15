0db5cbdfeefc1235e5644fbe7769d486c183f915
LouistheLasagna <louisyou08@gmail.com>
2022-03-15 09:50:10 +0900

    Help

--- src/MainPage/index.js
+++ src/MainPage/index.js
@@ -4,23 +4,29 @@ const Container=styled.div`
     height: 100vh;
     background-color: #28bec6;
     display: center;
-    flex-direction: row;
 `
 
 const Hello = styled.h1`
     font-size: 100px;
     color: white;
-    margin-left: 350px;
-    margin-top: 100px;
     text-align: center
 `
 
 const Desc = styled.h1`
     font-size: 30px;
     color: white;
-    margin-top: 500px;
-    margin-right: 700px;
     text-align: center
+`
+
+const Imgwrapper=styled.h1`
+    display: flex;
+    justify-content: center
+`
+
+const Youtube = styled.h1`
+    width: 100px;
+    height: 100px;
+    background-image: url("https://i.pinimg.com/originals/7d/c9/93/7dc993c70d4adba215b87cafdc59d82d.png")
 `
 
 function MainPage() {
@@ -28,6 +34,9 @@ function MainPage() {
     <Container>
         <Hello>Hello, I am Louis</Hello>
         <Desc>Competitive Fortnite Player | VFX</Desc>
+        <Imgwrapper>
+        <Youtube></Youtube>
+        </Imgwrapper>
     </Container>
     )
 }
